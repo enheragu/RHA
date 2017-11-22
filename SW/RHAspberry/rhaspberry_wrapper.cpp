c4ad6b6da1b20304c8621c2ae3c0aae1cfaf5d23
eeha <enrique.he.ag@gmail.com>
2017-11-22 17:19:28 +0100

    RHAspberryWrapper added

---
+++ SW/RHAspberry/rhaspberry_wrapper.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+#include "external/rapidxml/rapidxml.hpp"
+#include "debug/debug.h"
+
+#namespace RHAspberryWrapper {
+
+void setupRHAspberryOptions() {
+
+}
+
+void getConfigurationFile(std::string conf_file_name) {
+
+}
+
+}  // End RHAspberryWrapper namespace

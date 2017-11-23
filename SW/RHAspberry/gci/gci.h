92465cec9648be98258548c29dbde3aec1c59e54
enheragu <enrique.he.ag@gmail.com>
2017-11-23 09:05:35 +0100

    Update info of files

---
+++ SW/RHAspberry/gci/gci.h
@@ -0,0 +1,8 @@
+/**
+ * @Author: Enrique Heredia Aguado <enheragu>
+ * @Date:   22_Nov_2017
+ * @Project: RHA
+ * @Filename: gci.h
+ * @Last modified by:   enheragu
+ * @Last modified time: 23_Nov_2017
+ */
